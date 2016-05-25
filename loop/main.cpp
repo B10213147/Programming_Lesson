b03de60e39ca4c8287b1af6798abe98fafebe6f4
B10213147 <B10213147@yuntech.org.tw>
2016-05-25 22:04:50 +0200

    Search prime num V1

--- loop/main.cpp
+++ loop/main.cpp
@@ -26,5 +26,33 @@ int main()
     cout<<num1<<"!="<<answer1<<endl;
     cout<<num2<<"!="<<answer2<<endl;
 
+    cout<<"=====Prime====="<<endl;
+    int start_n, end_n;
+    cout<<"start=";
+    cin>>start_n;
+    cout<<"end=";
+    cin>>end_n;
+
+    if(start_n < 2){
+        start_n = 2;
+    }
+
+    int j;
+    bool flag;
+
+    for(i=start_n; i<=end_n; i++){
+        flag = true;
+        for(j=2; j<i; j++){
+            if(i%j == 0){
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag == true){
+            cout<<i<<" ";
+        }
+    }
+
     return 0;
 }
