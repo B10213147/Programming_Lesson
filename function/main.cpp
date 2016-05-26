031921f7f9d970b4b3b8b358bbb2819f124f760b
B10213147 <B10213147@yuntech.org.tw>
2016-05-26 21:10:41 +0200

    Finished min_Sort

--- function/main.cpp
+++ function/main.cpp
@@ -5,6 +5,9 @@
 int max2(int a, int b);
 int max3(int a, int b, int c);
 void max_Sort(int a, int b, int c);
+int min2(int a, int b);
+int min3(int a, int b, int c);
+void min_Sort(int a, int b, int c);
 
 using namespace std;
 
@@ -20,6 +23,9 @@ int main()
 
     cout<<"=====Max_Sort====="<<endl;
     max_Sort(x, y, z);
+
+    cout<<"=====Min_Sort====="<<endl;
+    min_Sort(x, y, z);
 
     return 0;
 }
@@ -60,6 +66,30 @@ void max_Sort(int a, int b, int c){
     }
 }
 
+void min_Sort(int a, int b, int c){
+    int out = min3(a, b, c);
+    cout<<out<<" ";
+
+    if(out == a){
+        out = min2(b, c);
+        cout<<out<<" ";
+        if(out == b) cout<<c<<endl;
+        else cout<<b<<endl;
+    }
+    else if(out == b){
+        out = min2(a, c);
+        cout<<out<<" ";
+        if(out == a) cout<<c<<endl;
+        else cout<<a<<endl;
+    }
+    else{
+        out = min2(a, b);
+        cout<<out<<" ";
+        if(out == a) cout<<b<<endl;
+        else cout<<a<<endl;
+    }
+}
+
 int max2(int a, int b){
     if(a >= b) return a;
     else return b;
@@ -70,5 +100,17 @@ int max3(int a, int b, int c){
     if(max_Num < b) max_Num = b;
     if(max_Num < c) max_Num = c;
     return max_Num;
+}
+
+int min2(int a, int b){
+    if(a <= b) return a;
+    else return b;
+}
+
+int min3(int a, int b, int c){
+    int min_Num = a;
+    if(min_Num > b) min_Num = b;
+    if(min_Num > c) min_Num = c;
+    return min_Num;
 }
 
