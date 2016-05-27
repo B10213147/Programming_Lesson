7da33c850e0f74aa68ac17cd913d761616840be8
B10213147 <B10213147@yuntech.org.tw>
2016-05-27 16:19:15 +0200

    Bubble sort

---
+++ sort/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#define size 50
+
+void swap_nums(int a, int b);
+
+using namespace std;
+
+int nums[size] = {size};
+
+int main()
+{
+    int i;
+    //create sequence numbers
+    for(i=1; i<size; i++){
+        nums[i] = nums[i-1] - 1;
+    }
+    cout<<"=====Before sort====="<<endl;
+    for(i=0; i<size; i++){
+        cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+
+    int j;
+    int compare=0, swap_n=0;
+
+    for(i=size-1; i>0; i--){
+        for(j=0; j<i; j++){
+            compare++;
+            if(nums[j] > nums[j+1]){
+                swap_n++;
+                swap_nums(j, j+1);
+            }
+        }
+    }
+
+    cout<<"=====After sort====="<<endl;
+    for(i=0; i<size; i++){
+        cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"compare="<<compare<<endl;
+    cout<<"swap="<<swap_n<<endl;
+
+    return 0;
+}
+
+void swap_nums(int a, int b){
+    int temp = nums[a];
+    nums[a] = nums[b];
+    nums[b] = temp;
+}
