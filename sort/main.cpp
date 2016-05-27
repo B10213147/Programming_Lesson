56b8dbd7e5c0c09aa47323b5ef97d5cdf7b92b58
B10213147 <B10213147@yuntech.org.tw>
2016-05-27 16:23:40 +0200

    Changed two variables to global

--- sort/main.cpp
+++ sort/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
-#define size 50
+#define size 10
 
 void swap_nums(int a, int b);
 
 using namespace std;
 
 int nums[size] = {size};
+int compare=0, swap_n=0;
 
 int main()
 {
@@ -15,6 +16,7 @@ int main()
     for(i=1; i<size; i++){
         nums[i] = nums[i-1] - 1;
     }
+
     cout<<"=====Before sort====="<<endl;
     for(i=0; i<size; i++){
         cout<<nums[i]<<" ";
@@ -22,13 +24,11 @@ int main()
     cout<<endl;
 
     int j;
-    int compare=0, swap_n=0;
-
+    //bubble sort
     for(i=size-1; i>0; i--){
         for(j=0; j<i; j++){
             compare++;
             if(nums[j] > nums[j+1]){
-                swap_n++;
                 swap_nums(j, j+1);
             }
         }
@@ -49,4 +49,5 @@ void swap_nums(int a, int b){
     int temp = nums[a];
     nums[a] = nums[b];
     nums[b] = temp;
+    swap_n++;
 }
